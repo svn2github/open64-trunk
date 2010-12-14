svn2github/open64-trunk

commit d5cbce4523b3c08f9d58913b18ff8e12dcfa2678

Fix bug649.
case:
1 namespace pcrecpp {
2 class Arg { public: Arg(); Arg(void*); };
3 class RE { static Arg no_arg; };
4 Arg RE::no_arg((void*)__null);
5 extern Arg no_arg __attribute__((alias("" "_ZN7pcrecpp2RE6no_argE")));
6 }
The wn for line5 is:
   U8U8LDID 0 <1,52,_ZN7pcrecpp2RE6no_argE> T<54,Arg,1>
it uses the base st(_ZN7pcrecpp2RE6no_argE)  instead of _ZN7pcrecpp6no_argE
the right one should be like this:
   U8LDA 0 <1,54,_ZN7pcrecpp6no_argE> T<55,anon_ptr.,8>

The fix is to add checking for alias node.

Reviewed by Jianxin.


git-svn-id: http://svn.open64.net/svnroot/open64/trunk@3432 ead328b1-e903-0410-a19d-eacee4b031f6

osprey/wgen/wgen_expr.cxx
@@ -2605,6 +2605,7 @@ WGEN_Address_Of(gs_t arg0)
   case GS_FUNCTION_DECL:
     {
       st = Get_ST (arg0);
+      ST *real_st = gs_decl_alias_target(arg0)? Get_ST(gs_decl_alias_target(arg0)):st;
       ty_idx = ST_type (st);
 #ifdef KEY
       // Arg0 is the virtual function table (vtable) for a class.  Initialize
@@ -2632,7 +2633,7 @@ WGEN_Address_Of(gs_t arg0)
 #endif
       // for VLAs, use the base_st instead of st
       if (code0 == GS_VAR_DECL &&
-          st != ST_base(st)) {
+          real_st != ST_base(st)) {
         FmtAssert (ST_ofst (st) == 0,
                    ("Variable Length Arrays within struct not currently implemented"));
         wn = WN_Ldid (Pointer_Mtype, 0, ST_base(st), ST_type(ST_base(st)));

commit 41e8dfc9dd8084a8fa8c1028e4d303e87adb4133

Fix problem caused by r3432. replace Get_ST() with DECL_ST(), the first one will create a new st if it is null.
Reviewed by Jianxin.


git-svn-id: http://svn.open64.net/svnroot/open64/trunk@3434 ead328b1-e903-0410-a19d-eacee4b031f6

osprey/wgen/wgen_expr.cxx
@@ -2605,7 +2605,8 @@ WGEN_Address_Of(gs_t arg0)
   case GS_FUNCTION_DECL:
     {
       st = Get_ST (arg0);
-      ST *real_st = gs_decl_alias_target(arg0)? Get_ST(gs_decl_alias_target(arg0)):st;
+      gs_t alias_target = gs_decl_alias_target(arg0);
+      ST *real_st = (alias_target && DECL_ST(alias_target))? DECL_ST(alias_target):st;
       ty_idx = ST_type (st);
 #ifdef KEY
       // Arg0 is the virtual function table (vtable) for a class.  Initialize