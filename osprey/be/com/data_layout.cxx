svn2github/open64-trunk

commit 00b8868b639fc992738ad683a7fb63621d9c3114

Fix bug703, add constraint at assertion "variable length struct's inito size should not be smaller than its' ST_size".
Code Review: Jian-xin 


git-svn-id: http://svn.open64.net/svnroot/open64/trunk@3492 ead328b1-e903-0410-a19d-eacee4b031f6

osprey/be/com/data_layout.cxx
@@ -652,7 +652,7 @@ Allocate_Space(ST *base, ST *blk, INT32 lpad, INT32 rpad, INT64 maxsize)
   INT64 size;
   INITO_IDX ino_idx;
   // if blk is variable length struct, its size should be inito size.
-  if (TY_kind(ST_type(blk)) == KIND_STRUCT && (ino_idx = Find_INITO_For_Symbol(blk)) != 0)
+  if (TY_kind(ST_type(blk)) == KIND_STRUCT && (ino_idx = Find_INITO_For_Symbol(blk)) != 0 && INITV_kind(INITO_val(ino_idx)) == INITVKIND_BLOCK)
   {
     size = Get_INITO_Size(ino_idx);
     Is_True(size >= ST_size(blk),("%s's inito size smaller than ST_size",ST_name(blk)));

commit 70758f56ccb5656f597ac1049f5803b664219808

Fix regression caused by r3492. Add constraint "ST_class(blk) == CLASS_VAR" before getting blk type idx, since ty_idx for blk is invalid if the sym_class is CLASS_FUNC or CLASS_BLOCK,CLASS_NAME.
Code Review: Sun 


git-svn-id: http://svn.open64.net/svnroot/open64/trunk@3553 ead328b1-e903-0410-a19d-eacee4b031f6

osprey/be/com/data_layout.cxx
@@ -652,7 +652,10 @@ Allocate_Space(ST *base, ST *blk, INT32 lpad, INT32 rpad, INT64 maxsize)
   INT64 size;
   INITO_IDX ino_idx;
   // if blk is variable length struct, its size should be inito size.
-  if (TY_kind(ST_type(blk)) == KIND_STRUCT && (ino_idx = Find_INITO_For_Symbol(blk)) != 0 && INITV_kind(INITO_val(ino_idx)) == INITVKIND_BLOCK)
+  if ( ST_class(blk) == CLASS_VAR &&
+       TY_kind(ST_type(blk)) == KIND_STRUCT && 
+       (ino_idx = Find_INITO_For_Symbol(blk)) != 0 &&
+       INITV_kind(INITO_val(ino_idx)) == INITVKIND_BLOCK )
   {
     size = Get_INITO_Size(ino_idx);
     Is_True(size >= ST_size(blk),("%s's inito size smaller than ST_size",ST_name(blk)));