svn2github/open64-trunk

commit dafd81adb924b6e6586f94ea019f6d6efdc8ee66

fix bug 878 and bug 670. Since gnu allows aliased symbol to be different storage and type from the base, so change to be compatible with gnu. Output warning for these cases.
Code Review:
Lai Jian-xin and Sun Chan.


git-svn-id: http://svn.open64.net/svnroot/open64/trunk@3758 ead328b1-e903-0410-a19d-eacee4b031f6

osprey/wgen/wgen_decl.cxx
@@ -4723,7 +4723,6 @@ WGEN_Assemble_Alias (gs_t decl, gs_t target)
   else {
     Set_ST_base_idx (st, ST_st_idx (base_st));
     Set_ST_emit_symbol(st);	// for cg
-    Set_ST_sclass (st, ST_sclass (base_st));
     if (ST_is_initialized (base_st))
       Set_ST_is_initialized (st);
 #ifdef KEY
@@ -4734,9 +4733,12 @@ WGEN_Assemble_Alias (gs_t decl, gs_t target)
 #ifdef KEY
   if (!lang_cplus)
   {
-    // bug 4981: symbol class of ST must match that of the target
-    if (ST_sym_class (st) != ST_sym_class (base_st))
+    if (ST_sym_class (st) != ST_sym_class (base_st)) {
+      /* open64.net bug 878, change the aliased sym class and type to the base st */
       ErrMsg (EC_Ill_Alias, ST_name (st), ST_name (base_st));
+      Set_ST_class(st, ST_class(base_st));
+      Set_ST_type(st,ST_type(base_st));
+    }
 
     // bugs 5145, 11993
     if (ST_sym_class (base_st) == CLASS_FUNC)

osprey/common/com/erglob.desc
@@ -417,12 +417,15 @@ ERROR_DESC EDESC_Globals[] = {
 #ifdef KEY
   { EC_Bad_Omp,		EM_User | ES_ERRABORT,		RAG_EN_NONE,
     "%s", 1, ET_STRING, 0, 0, 0, 0, 0 },
-  { EC_Ill_Alias,	EM_User | ES_ERRABORT,		RAG_EN_NONE,
+  { EC_Ill_Alias,	EM_User | ES_WARNING,		RAG_EN_NONE,
     "Declaration of alias \"%s\" does not match its target \"%s\".",
     2, ET_STRING, ET_STRING, 0, 0, 0, 0 },
   { EC_Misc_Asm,	EM_User | ES_ERRABORT,		RAG_EN_NONE,
     "%s", 1, ET_STRING, 0, 0, 0, 0, 0 },
 #endif /* KEY */
+  { EC_Sym_Sto_Cla,     EM_User | ES_WARNING , RAG_EN_NONE,
+    "Storage of alias \"%s\" should be identical as its target \"%s\".",
+    2, ET_STRING, ET_STRING, 0, 0, 0, 0 },
 
   { EC_Ill_Align,	EM_User | ES_WARNING ,	RAG_EN_NONE,
     "Illegal alignment (%d) in %s",

osprey/common/com/erglob.h
@@ -350,6 +350,9 @@ static char *erglob_rcs_id = "$Source: common/com/SCCS/s.erglob.h $ $Revision: 1
 #define EC_Inv_Asm_Opnd EC_Undef_Code+340       /* none */
 #define EC_Misc_Asm EC_Undef_Code+341		/* str */
 #endif
+/* Symtab Alias Storage Verfication: */
+#define EC_Sym_Sto_Cla EC_Undef_Code+354        /* str,str */
+
 
 
 /* ====================================================================
@@ -476,6 +479,7 @@ static char *erglob_rcs_id = "$Source: common/com/SCCS/s.erglob.h $ $Revision: 1
 
 /* IPA dladd error codes: */
 #define EC_IP_Load_Dso  EC_BASE_FILE+120    /* str, str */
+
 
 #ifdef __cplusplus
 }

osprey/common/com/symtab_verify.cxx
@@ -59,6 +59,7 @@
 #endif
 
 #include "symtab_defs.h"
+#include "erglob.h"
 
 // ======================================================================
 // Auxiliary functions used by ST::Verify()
@@ -514,9 +515,13 @@ ST_Verify_Fields(const ST &s)
     if ( ST_storage_class (*sb) != SCLASS_UNKNOWN) {
 
       if ( !ST_is_weak_symbol (s) )
-        Is_True( ST_storage_class(s) == ST_storage_class(*sb),
-                 (msg,"storage class, should be identical as based blocks"));
-
+        {
+          /* open64.net bug878, aliased symbol storage class may not be equal 
+             to the base. */
+          if (ST_storage_class(s) != ST_storage_class(*sb)) {
+            ErrMsg(EC_Sym_Sto_Cla, ST_name(s), ST_name(sb));
+          }
+        }
     }
     else {
       // Property 1b For blocks such as bss, SCLASS== UNKNOWN and EXPORT==LOCAL

commit 8179be47fd9b3f33985627393ba4278a0dbf9958

fix bug924 and the regression(20040323-1) caused by r3758
successive patch on alias attribute handling, i.e, right setup the alias
symbol storage.

Code Review: Lai Jian-Xin.


git-svn-id: http://svn.open64.net/svnroot/open64/trunk@3846 ead328b1-e903-0410-a19d-eacee4b031f6

osprey/be/cg/cgemit.cxx
@@ -8267,7 +8267,9 @@ Process_Bss_Data (SYMTAB_IDX stab)
 		    ST_base_idx(sym) != ST_st_idx(sym) &&
 		    !ST_is_equivalenced(sym) &&
 		    ST_class(ST_base(sym)) != CLASS_BLOCK &&
-                    pu != NULL && ! PU_ftn_lang(*pu) /* bug 13585 */)
+                    // originally bug 13585. bug 924 open64.net.
+                    // no pu case should also be considered.
+                    ((!pu) || !PU_ftn_lang (*pu)))
 		  goto skip_definition;
 #endif
 		size = TY_size(ST_type(sym));

osprey/wgen/wgen_decl.cxx
@@ -4723,12 +4723,28 @@ WGEN_Assemble_Alias (gs_t decl, gs_t target)
   else {
     Set_ST_base_idx (st, ST_st_idx (base_st));
     Set_ST_emit_symbol(st);	// for cg
-    if (ST_is_initialized (base_st))
+    if (ST_is_initialized (base_st)) {
       Set_ST_is_initialized (st);
+      // bug924 open64.net. global alias symbol with base initialized
+      // should set storage class SCLASS_DGLOBAL.
+      if (ST_sclass(st) == SCLASS_COMMON || ST_sclass(st) == SCLASS_UGLOBAL) 
+        Set_ST_sclass (st, SCLASS_DGLOBAL);
+    }
 #ifdef KEY
-    if (ST_init_value_zero (base_st))
+    if (ST_init_value_zero (base_st)) {
       Set_ST_init_value_zero (st);
+      // bug924 open64.net. Those base initialized zero
+      // symbols should restore to SCLASS_UGLOBAL.
+      if (ST_sclass(st) == SCLASS_DGLOBAL)
+        Set_ST_sclass (st, SCLASS_UGLOBAL);
+    }
 #endif
+    // bug924 open64.net aliased symbols in COMMON section should be 
+    // set to be uninitialized. Since aliased symbols themselves 
+    // should never be allocated.
+    if (ST_sclass(st) == SCLASS_COMMON) {
+      Set_ST_sclass (st, SCLASS_UGLOBAL);
+    }
   }
 #ifdef KEY
   if (!lang_cplus)